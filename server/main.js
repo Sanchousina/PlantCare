c34c79a9fc2bbe4f72286d12aa6f6d8465400733
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-01-09 15:14:25 +0100

    Implement GET all measurements route

--- server/main.js
+++ server/main.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
+const measurements = require('../data.json');
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../public')));
+
+app.get('/api/measurements/', (req, res) => {
+  res.send(measurements);
+})
 
 app.listen(8000, () => {
   console.log('Server is listening');
