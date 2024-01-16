1aaf50957f6a503a8e57dfb5c7b5879a1d1913f3
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-01-16 14:22:03 +0100

    Implement POST plants route

--- server/main.js
+++ server/main.js
@@ -31,6 +31,23 @@ app.use('/', express.static(path.join(__dirname, '../public')));
 
 
 //API Endpoints
+app.post('/api/plants/', (req, res) => {
+  const query = `INSERT INTO Plant 
+  (plant_name, temperature_min, temperature_max, moisture_min, moisture_max, fertility_min, fertility_max, light_min, light_max)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const {plant_name, temperature_min, temperature_max, moisture_min, moisture_max, fertility_min, fertility_max, light_min, light_max} = req.body;
+  db.run(query, [plant_name, temperature_min, temperature_max, moisture_min, moisture_max, fertility_min, fertility_max, light_min, light_max], (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(201).json({
+        data: {
+          status: 'success'
+        }
+      })
+    }
+  })
+})
 app.get('/api/measurements/', (req, res) => {
   res.send(measurements);
 })
--- server/sqlTables/createPlantTable.sql
+++ server/sqlTables/createPlantTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Plant (
   plaint_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
+  plant_name TEXT NOT NULL,
   temperature_min REAL NOT NULL, 
   temperature_max REAL NOT NULL,
   moisture_min REAL NOT NULL,
